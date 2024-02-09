78f6fc34f2ffe070d89e289002760caad20d5b71
Globlobglob <122725189+Globlobglob@users.noreply.github.com>
2024-02-09 09:48:32 +0200

    Add files via upload

---
+++ 215_ovcharuk_botnar/Practical_task_2/Practical_task_2/Employe.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_task_2
+{
+    class Employe:Person
+    {
+
+        string company;
+        public string Company { get => company; set => company = value; }
+
+        string job;
+            
+        public string Job { get => job; set => job = value; }
+
+        public Employe() { }
+
+        public Employe(int age,string name,string surname,string company,string job):base(age,name,surname)
+        {
+            this.company = company;
+            this.job = job;
+        }
+
+        public void Print
+    }
+}
